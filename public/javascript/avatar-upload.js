200177d517683ec4bb439a52cfdcc3d27001f258
Jeff Johnston <jeffwjohn@yahoo.com>
2020-11-12 12:57:32 -0600

    randomize file name

--- public/javascript/avatar-upload.js
+++ public/javascript/avatar-upload.js
@@ -38,3 +38,50 @@
 //   (() => {
 //     document.getElementById('file-input').onchange = signup;
 // })();
+
+  //Bucket Configurations
+  var bucketName = 'cloud-rolodex';
+  var bucketRegion = 'us-east-2';
+  var IdentityPoolId = 'us-east-2:6b47a7ae-f625-4149-91b1-bf2a68223673';
+
+  AWS.config.update({
+    region: bucketRegion,
+    credentials: new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: IdentityPoolId
+    })
+  });
+
+  var s3 = new AWS.S3({
+    apiVersion: '2006-03-01',
+    params: { Bucket: bucketName }
+  });
+
+
+
+function s3upload() {
+   var files = document.getElementById('fileUpload').files;
+   if (files) 
+   {
+     var file = files[0];
+     // Save to a random filename to prevent file tampering
+     var fileName = Math.random().toString().substr(2);
+     var filePath = 'my-first-bucket-path/' + fileName;
+     var fileUrl = 'https://' + bucketRegion + '.amazonaws.com/my-    first-bucket/' +  filePath;
+     s3.upload({
+        Key: filePath,
+        Body: file,
+        ACL: 'public-read'
+        }, function(err, data) {
+        if(err) {
+        reject('error');
+        }
+        alert('Successfully Uploaded!');
+        }).on('httpUploadProgress', function (progress) {
+        var uploaded = parseInt((progress.loaded * 100) / progress.total);
+        $("progress").attr('value', uploaded);
+      });
+   }
+};
+
+
+   
