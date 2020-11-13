a94346a0c32a55b025f7f043b9d3d1aed5e53c7e
Jeff Johnston <jeffwjohn@yahoo.com>
2020-11-12 19:10:06 -0600

    set seq sync to false

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({
-  force: true
+  force: false
 }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
