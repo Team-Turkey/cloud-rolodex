59daff03dc5e59d0bae330fc043040741625a524
Seth Uschuk <suschuk24@gmail.com>
2020-11-12 19:23:31 -0600

    re-added dashboard.js file that Jeff deleted, totally his fault

---
+++ public/javascript/dashboard.js
@@ -0,0 +1,49 @@
+var searchAllUsersEl = document.querySelector("#all-users")
+var nameInputEl = document.querySelector("#first-name")
+var usersContainerEl = document.querySelector("#users-container");
+
+var departmentButtonsEl = document.querySelector("#department-buttons")
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    var name = nameInputEl.value.trim()
+
+    if (name) {
+        
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter your co-workers first name!")
+    }
+};
+
+
+
+
+function buttonClickHandler(event) {
+    var iframeContainerEl = document.createElement("div")
+    console.log("iframe container element", iframeContainerEl)
+    var name = event.target.getAttribute("id")
+    console.log("name", name)
+    // create an iframe element
+    var iframeEL = document.getElementById("myframe")
+
+    // set src attribute
+    iframeEL.setAttribute("src", '../dashboard/' + name + '/')
+    
+    
+    console.log(iframeEL)
+    
+    
+};
+
+function UpdateButtonClickHandler(event) {
+    event.preventDefault()
+    document.location.replace('/edit-user')
+}
+
+
+
+searchAllUsersEl.addEventListener("click", buttonClickHandler)
+
+departmentButtonsEl.addEventListener("click", buttonClickHandler);
+
