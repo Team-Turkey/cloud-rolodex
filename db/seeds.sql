18a59d8f6c0dd9e55256579daa38797a452b5c99
Jeff Johnston <jeffwjohn@yahoo.com>
2020-11-13 19:39:29 -0600

    fix db

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO user
+  (username, avatar, role_id, first_name, last_name, phone, email, password)
+VALUES
+  ('jeebs','/images/default.png', 1, 'Ronald', 'Firbank', '555-555-5555', 'jamie@jeebs.com', 'password');
+
+
+ 
